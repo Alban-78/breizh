62e5ce559858280cc60c040643725ee21c275a9a
unknown <albanhusar@gmail.com>
2020-06-15 15:58:33 +0200

    branch20

--- app/views/FrontEnd/layouts/footer.php
+++ app/views/FrontEnd/layouts/footer.php
@@ -20,12 +20,12 @@
         </div>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- <script src="app/public/js/modal.js"></script>
+    <script src="app/public/js/modal.js"></script>
     <script src="app/public/js/cookie.js"></script>
     <script src="app/public/js/slider.js"></script>
     <script src="app/public/js/apigeo.js"></script>
-    <script src="app/public/js/cartealacarte.js"></script> -->
-    <script src="app/public/js/opti.js"></script>
+    <script src="app/public/js/cartealacarte.js"></script>
+    <!-- <script src="app/public/js/opti.js"></script> -->
     
 
 </body>
